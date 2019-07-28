52ce41bf83559cfea5199d82a016b2e3202aa74e
Vijayalakshmi <vijayalakshmitce@gmail.com>
2019-07-28 09:04:06 -0400

    added heroku connection cerdential

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,24 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Sairakshna5*",
-    database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "Sairakshna5*",
+//     database: "burgers_db"
+// });
+
+var connection;
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+    connection = mysql.createConnection({
+        host:'localhost',
+        uses: 'root',
+        password: 'hacktheplanet',
+        database: 'todoagain_db'
+    });
+};
+connection.connect();
 
 module.exports = connection;
